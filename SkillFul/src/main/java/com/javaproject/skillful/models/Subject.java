d9a8d963f4b015a920e950c0754432d550fd7b09
Yosef Assi <yassi3766@gmail.com>
2022-11-20 11:20:46 +0200

    Subject

--- SkillFul/src/main/java/com/javaproject/skillful/models/Subject.java
+++ SkillFul/src/main/java/com/javaproject/skillful/models/Subject.java
@@ -1,5 +1,45 @@
 package com.javaproject.skillful.models;
 
-public class Subject {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name="subjects")
+public class Subject {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	
+	@NotEmpty(message="Title is required!")
+	@Size(min=3, max=30, message="Title must be between 3 and 30 characters")
+	private String title;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	
 }
